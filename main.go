7ab7dd2c699700e6fdf94e2089c494c3939f56ed
Ed <nnichego7@gmail.com>
2025-11-05 17:36:06 +0300

    Go

--- main.go
+++ main.go
@@ -1,36 +1,20 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-func divide(a, b int) (int, error) {
-	if b == 0 {
-		return 0, errors.New("нельзя делить на ноль")
+func Sqrt(x float64) float64 {
+	z := x / 2
+	y := 0.0
+	for z-y > 0.001 {
+		y = z
+		z -= (z*z - x) / (2 * z)
 	}
-	return a / b, nil
-}
+	return z
 
-func validateAge(age int) error {
-	if age == 0 {
-		return errors.New("некорректный возраст")
-	}
-	return nil
 }
 
 func main() {
-	result, err := divide(10, 0)
-	if err != nil { // Если ошибка не nil, то есть если есть ошибка
-		fmt.Println("Ошибка:", err)
-	} else {
-		fmt.Println("Результат:", result)
-	}
-	err = validateAge(0)
-	if err != nil {
-		fmt.Println("Ошибка:", err)
-	} else {
-		fmt.Println("Возраст корректен")
-
-	}
+	fmt.Println(Sqrt(1))
 }
