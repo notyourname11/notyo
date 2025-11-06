88df848776d1a000e33251d0c0fcab34bdfaf612
Ed <nnichego7@gmail.com>
2025-11-06 13:34:08 +0300

    Task 2

--- main.go
+++ main.go
@@ -2,21 +2,13 @@ package main
 
 import "fmt"
 
-// SumInts возвращает сумму всех чисел nums.
-// Если срез пуст, должна вернуться 0.
-func SumInts(nums []int) int {
-	SumInts := 0
-	for index, _ := range nums {
-		SumInts += nums[index]
-	}
-	return SumInts
-
-}
-
 func main() {
-	fmt.Println(SumInts([]int{1, 2, 3}))
-	fmt.Println(SumInts([]int{}))
-	// Ожидаемый вывод:
-	// 6
-	// 0
+	in := []int{5, 4, 9, 2, 7, 6}
+	for i := len(in) - 1; i >= 0; i-- {
+		if in[i]%2 != 0 {
+			in = append(in[:i], in[i+1:]...)
+		}
+	}
+	fmt.Println((in))
+
 }
